4efe7dc3e78d2591f9eff8c14a4fc5c698836436
Anna Hilla <annahilla21@gmail.com>
2024-11-15 21:00:27 +0100

    Add margin on header and change font size for better responsive design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,7 @@
-const Header = ({ title }:{ title:string }) => {
+const Header = ({ title }: { title: string }) => {
   return (
-    <header className="my-8 flex items-center justify-center text-center border-3xl h-80 bg-header bg-center bg-cover rounded-3xl">
-      <h1 className="text-2xl sm:text-3xl md:text-5xl font-bold">
-        {title}
-      </h1>
+    <header className="my-8 px-5 flex items-center justify-center text-center border-3xl h-80 bg-header bg-center bg-cover rounded-3xl">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold">{title}</h1>
     </header>
   );
 };
